2942dfaa0bf9d830312f84b60de836c22e788083
Demoniolik <mdidenko27@gmail.com>
2022-08-15 17:27:47 +0300

    add and style 3 new section

--- src/js/script.js
+++ src/js/script.js
@@ -13,16 +13,15 @@ $(document).ready(function () {
 });
 
 $(document).ready(function () {
-    // $(".slide-current").lastChild
     $(".slider").slick({
         centerPadding: "40px",
         slidesToShow: 3,
         slidesToScroll: 1,
         variableWidth: true,
         prevArrow:
-            '<button type="button" class="slick-prev"><img  src="src/img/galery/prev-arrow.png" alt="slide"></button>',
+            '<button type="button" class="slick-prev"><img  src="src/img/galery/arrow-prev.png" alt="slide"></button>',
         nextArrow:
-            '<button type="button" class="slick-next"><img  src="src/img/galery/next-arrow.png" alt="slide"></button>',
+            '<button type="button" class="slick-next"><img  src="src/img/galery/arrow-next.png" alt="slide"></button>',
         dots: false,
         responsive: [
             {
@@ -46,5 +45,54 @@ $(document).ready(function () {
         prevElement.removeClass("active");
         let currentSlide = $(".slick-current");
         prevElement = setSlideTextActive(currentSlide);
+    });
+});
+
+$(document).ready(function () {
+    $(".cities__wrapper").slick({
+        centerPadding: "40px",
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        variableWidth: true,
+        prevArrow:
+            '<button type="button" class="slick-prev"><img  src="src/img/galery/arrow-prev.png" alt="slide"></button>',
+        nextArrow:
+            '<button type="button" class="slick-next"><img  src="src/img/galery/arrow-next.png" alt="slide"></button>',
+        dots: false,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 1,
+                    centerMode: true,
+                    dots: true,
+                },
+            },
+        ],
+    });
+});
+
+$(document).ready(function () {
+    $(".reviews__wrapper").slick({
+        centerPadding: "40px",
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        variableWidth: true,
+        prevArrow:
+            '<button type="button" class="slick-prev"><img  src="src/img/galery/arrow-prev.png" alt="slide"></button>',
+        nextArrow:
+            '<button type="button" class="slick-next"><img  src="src/img/galery/arrow-next.png" alt="slide"></button>',
+        dots: false,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    dots: true,
+                },
+            },
+        ],
     });
 });
